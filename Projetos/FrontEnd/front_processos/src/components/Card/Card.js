bda69d190d09a49f0962d07a4e4bb5a8d90c0444
Cristian Ogliari <cogliari@gmail.com>
2021-07-16 20:49:03 -0300

    criacao: elementos do card

--- Projetos/FrontEnd/front_processos/src/components/Card/Card.js
+++ Projetos/FrontEnd/front_processos/src/components/Card/Card.js
@@ -1,23 +1,29 @@
 import { CardContent, CardMedia, Grid, Typography } from '@material-ui/core';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Card from '@material-ui/core/Card';
 import CreateIcon from '@material-ui/icons/Create';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 import { useStyles } from "./Card.style";
 import card_img from '../../assets/images/card_img.png';
+import { useState } from 'react';
 
 export const ProcessoCard = () => {
+  const [expanded, setExpanded] = useState(false);
   const classes = useStyles();
 
-  const handleOpen = () => {
-    return null;
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
   }
 
   return (
     <Card className={classes.root}>
       <CardContent className={classes.main}>
         <Grid container spacing={0}>
-          <Grid item xs={2} className={classes.gridCardStyle} >
+          <Grid item xs={2} >
             <CardContent className={classes.mainImage}>
               <CardMedia 
                 className={classes.image}
@@ -26,7 +32,7 @@ export const ProcessoCard = () => {
             </CardContent>
           </Grid>
           <Grid item xs={8}>
-            <CardContent>
+            <CardContent className={classes.gridTypoSize}>
               <Grid container spacing={0}>
                 <Grid item xs={4} className={classes.gridCardStyle} >
                   <CardContent className={classes.gridTypoSize}>
@@ -40,7 +46,7 @@ export const ProcessoCard = () => {
                     </Typography>
                   </CardContent>
                 </Grid>
-                <Grid item xs={3} className={classes.gridCardStyle} >
+                <Grid item xs={3}>
                   <CardContent className={classes.gridTypoSize}>
                     <Typography className={classes.title}>
                       Data
@@ -54,20 +60,71 @@ export const ProcessoCard = () => {
                 </Grid>
               </Grid>
             </CardContent>
-            <CardContent>
-              <h5>teste</h5>
+            <CardContent className={classes.gridTypoSize}>
+              <Typography className={classes.title}>
+                Assunto
+              </Typography>
+            </CardContent>
+            <CardContent className={classes.gridTypoSize}>
+              <Typography className={classes.subtitle}>
+                Corte de Árvores Frutíferas
+              </Typography>
             </CardContent>
           </Grid>
           <Grid item xs={2}>
-            <CardContent className={classes.iconStyle}>
-              <CreateIcon fontSize='small' />
-              <DeleteIcon fontSize='small' />
+            <CardContent className={classes.gridTypoSize}>
+              <CardContent className={classes.gridIconSize}>
+                <CreateIcon fontSize='small' style={{ color: '#666666' }} />
+                <DeleteIcon fontSize='small' style={{ color: '#666666' }} />
+                <CardActions disableSpacing style={{ padding: '0' }}>
+                  <IconButton
+                    className={classes.expand, {
+                      [classes.expandOpen]: expanded,
+                    }}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    fontSize='small'
+                    style={{ padding: '0' }}
+                    aria-label="show more" >
+                    <ExpandMoreIcon />
+                  </IconButton>
+                </CardActions>
+              </CardContent>
             </CardContent>
           </Grid>          
         </Grid>      
       </CardContent>
       <CardContent className={classes.moreInfo}>
-
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent className={classes.moreInfo}>
+            <Grid container spaceing={0}>
+              <Grid item xs={3}>
+                <CardContent className={classes.gridTypoSize}>
+                  <Typography className={classes.title}>
+                    Interessado
+                  </Typography>
+                </CardContent>
+                <CardContent className={classes.gridTypoSize}>
+                  <Typography className={classes.subtitle}>
+                    Zé da Esquina
+                  </Typography>
+                </CardContent>
+              </Grid>
+              <Grid item xs={9}>
+                <CardContent className={classes.gridTypoSize}>
+                  <Typography className={classes.title}>
+                    Descrição
+                  </Typography>
+                </CardContent>
+                <CardContent className={classes.gridTypoSize}>
+                  <Typography className={classes.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </Typography>
+                </CardContent>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Collapse>
       </CardContent>
     </Card>
   )
--- Projetos/FrontEnd/front_processos/src/components/Card/Card.style.js
+++ Projetos/FrontEnd/front_processos/src/components/Card/Card.style.js
@@ -1,39 +1,58 @@
 import { makeStyles } from "@material-ui/core";
+import { FullscreenExit } from "@material-ui/icons";
 
-export const useStyles = makeStyles({
+export const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: '50%',
     margin: 'auto',
     border: '1px solid #c6c6c675',
     borderRadius: '5px',
     boxShadow: 'none',
-    padding: 0,
       '&:last-child': {
         paddingBottom: 0,
       },
   }, 
-  logoProcesso: {
-    
-  },
-  title: `
-    font-weight: bold;
-    font-size: 0.8rem;
-  `,
-
-  // title: { 
-  //   fontSize: '0.9rem'
-  // },
+  title: {
+    marginTop: '10px',
+    fontWeight: '600',
+    fontSize: '0.8rem',
+    color: '#666666',
+  }, 
   subtitle: {
-    fontSize: '1rem'
+    fontSize: '0.9rem',
+    color: '#666666',
   },
   text: {
-
+    fontSize: '0.9rem',
+    color: '#666666',
+    textAlign: 'justify',
   },
   main: {
-    padding: 0,
+    padding: '0px 0px 5px 0px',
   },
   gridCardStyle: {
     padding: 0,
+      '&:last-child': {
+        paddingBottom: 0,
+      },
+  },
+  gridTypoSize: {
+    display: 'flex',
+    width: '100%',
+    padding: 0,
+      '&:last-child': {
+        paddingBottom: 0,
+      },
+  },
+  gridIconSize: {
+    display: 'flex',
+    marginTop: '10px',
+    marginLeft: 'auto',
+    marginRight: '10px',
+    padding: 0,
+      '&:last-child': {
+        paddingBottom: 0,
+      },
   },
   mainImage: {
     width: '90px',
@@ -49,18 +68,27 @@ export const useStyles = makeStyles({
     width: '100%',
     height: '100%',
   },
-  gridTypoSize: {
-    padding: 0,
-      '&:last-child': {
-        paddingBottom: 0,
-      },
-
-  },
   iconStyle: {
     display: 'flex',
     justifyContent: 'end',
   },
   moreInfo: {
-
+    paddingBottom: 0,
+      '&:last-child': {
+        paddingTop: 0,
+        paddingBottom: 0,
+        marginTop: '5px',
+        marginBottom: '5px',
+      },
   },
-})
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.short,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}))
