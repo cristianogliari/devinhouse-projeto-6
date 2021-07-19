ed5a355537794b4cc1b148ff13dbeaa2b9f6ec15
Cristian Ogliari <cogliari@gmail.com>
2021-07-19 20:02:35 -0300

    correcao: endpoints backend de processos corrigidos

--- Projetos/FrontEnd/front_processos/src/utils/axios/AxiosLogin.js
+++ Projetos/FrontEnd/front_processos/src/utils/axios/AxiosLogin.js
@@ -5,10 +5,11 @@ import { BASE_URL } from '../services/BASE_URL';
 class AxiosLogin {
   async autenticarUsuario(data) {
     try {
+      console.log(data);
       const response = await axios.post(`${BASE_URL.BASE_KEYCLOAK}`, {
         'grant_type' : 'password',
-        'client_id' : 'projeto6-back',
-        'client_secret' : 'e227f271-398d-49de-bbeb-2fc27b632f02',
+        'client_id' : 'projeto6-front',
+        'client_secret' : '8988e5ba-666a-4aa9-910d-377a4c67ca07',
         'username' : `${data.username}`,
         'password' : `${data.password}`
       }, {
