89f109379eb39bb7398168af90034e79ab92d36e
David Mendonça <davidmed66@gmail.com>
2021-07-20 19:27:41 -0300

    feat: skeleton

--- Projetos/FrontEnd/front_processos/src/pages/Home/index.js
+++ Projetos/FrontEnd/front_processos/src/pages/Home/index.js
@@ -1,76 +1,84 @@
+import { useState, useEffect } from "react";
+
+import { Paper } from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
+
 import { Navbar } from "../../components/Navbar";
 import { ModalFormulario } from "../../components/ModalFormulario";
 import { ProcessoCard } from "../../components/Card";
-import Skeleton from "@material-ui/lab/Skeleton";
-import { useState } from "react";
 import { AddProcessButton } from "../../components/AddButton/AddButton";
-import { Paper } from "@material-ui/core";
-
-const skeleton = false;
 
 export const Home = () => {
   const [openModal, setOpenModal] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleModalState = () => {
     setOpenModal((prev) => !prev);
   };
 
   const handleEdit = () => {
-    setOpenModal(true)
-  }
+    setOpenModal(true);
+  };
+
+  useEffect(() => {
+    /* fetchProducts().then((state) => {
+      setProducts(state);
+      setLoading(!loading);
+    }); */
+  }, []);
 
   return (
     <>
       <Navbar />
-      
-      <Paper elevation={3} style={{ width: '55%', margin: 'auto', paddingTop: '50px', paddingBottom: '10px' }}>
-        {skeleton ? (
+
+      <Paper
+        elevation={3}
+        style={{
+          width: "55%",
+          margin: "auto",
+          paddingTop: "50px",
+          paddingBottom: "10px",
+        }}
+      >
+        {loading ? (
           <Skeleton
-            width="50%"
+            width="80%"
             height="181px"
             animation="wave"
             style={{ margin: "auto", borderRadius: "10px" }}
           />
         ) : (
-          <ProcessoCard 
-            handleEdit={handleEdit}
-          />
+          <ProcessoCard handleEdit={handleEdit} />
         )}
 
-{skeleton ? (
+        {loading ? (
           <Skeleton
-            width="50%"
+            width="80%"
             height="181px"
             animation="wave"
             style={{ margin: "auto", borderRadius: "10px" }}
           />
         ) : (
-          <ProcessoCard 
-            handleEdit={handleEdit}
-          />
+          <ProcessoCard handleEdit={handleEdit} />
         )}
 
-{skeleton ? (
+        {loading ? (
           <Skeleton
-            width="50%"
+            width="80%"
             height="181px"
             animation="wave"
             style={{ margin: "auto", borderRadius: "10px" }}
           />
         ) : (
-          <ProcessoCard 
-            handleEdit={handleEdit}
-          />
+          <ProcessoCard handleEdit={handleEdit} />
         )}
-
-
       </Paper>
-      
-      <AddProcessButton openModalCadastro={handleModalState}/>
 
-      <ModalFormulario 
-      openModal={openModal}
-      handleModalState={handleModalState}
+      <AddProcessButton openModalCadastro={handleModalState} />
+
+      <ModalFormulario
+        openModal={openModal}
+        handleModalState={handleModalState}
       />
     </>
   );
