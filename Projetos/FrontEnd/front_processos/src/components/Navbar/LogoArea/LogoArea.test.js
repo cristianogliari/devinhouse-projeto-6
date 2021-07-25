0840f75be9e9f744c192d527074abe9ef1756b9e
David Mendonça <davidmed66@gmail.com>
2021-07-25 17:21:46 -0300

    test: LogoArea

--- Projetos/FrontEnd/front_processos/src/components/Navbar/LogoArea/LogoArea.test.js
+++ Projetos/FrontEnd/front_processos/src/components/Navbar/LogoArea/LogoArea.test.js
@@ -8,26 +8,23 @@ import { LogoArea } from "./LogoArea";
 const renderComponent = () => render(<LogoArea />)
 
 describe("LogoArea Component", () => {
-    beforeEach(() => {
-        renderComponent();
-    });
 
     it("Deve renderizar o nome DEVinHouse", () => {
-
+        renderComponent();
         expect(screen.getByText("DEVinHouse")).toBeInTheDocument();
     })
     
     it("Deve renderizar o foguete branco da logo", () => {
-        //const imgSource = require("../../../assets/rocketlaunch.svg");
+        renderComponent();
         const img = screen.getByAltText("rocketlaunchwhite");
         expect(img.getAttribute("src")).toContain(rocketlaunchwhite);
     })
 
     /* APRENDER COMO FAZER TESTE EM CASO DE TERNÁRIO */
     //TODO: Fazer o teste do foguete preto do dark theme
-    it("Deve renderizar o foguete preto da logo", () => {
-        //const imgSource = require("../../../assets/rocketlaunch.svg");
-        const img = screen.getByAltText("rocketlaunchwhite");
+    /* it("Deve renderizar o foguete preto da logo", () => {
+        customRender();
+        const img = screen.getByAltText("rocketlaunch");
         expect(img.getAttribute("src")).toContain(rocketlaunchwhite);
-    })
+    }) */
 })
