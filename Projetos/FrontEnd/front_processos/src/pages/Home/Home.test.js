b00947bf76f600b8d6fb774bdca2e9f5bcd37bd2
Cristian Ogliari <cogliari@gmail.com>
2021-07-25 21:58:34 -0300

    criacao: teste home page

--- Projetos/FrontEnd/front_processos/src/pages/Home/Home.test.js
+++ Projetos/FrontEnd/front_processos/src/pages/Home/Home.test.js
@@ -1,37 +1,54 @@
 import { render, screen } from "@testing-library/react";
 
 import { Home } from "./index";
+import { useDataContext } from "../../utils/context/DataContext";
+// jest.mock("../../utils/context/DataContext");
 
-const renderComponent = () => render(
-    <Home/>)
-    
-    describe("Avatar Component", () => {
-        it("Deve renderizar Navbar", () => {
-            renderComponent();
-            expect(screen.getByText("DEVinHouse")).toBeInTheDocument();
-        })
+jest.mock("../../utils/context/DataContext", () => ({
+	...jest.requireActual("../../utils/context/DataContext"),
+	useDataContext: () => ({
+		listaProcesso: [],
+		listaAssunto: [],
+		listaInteressado: [],
+		recarregarProcessos: jest.fn().mockImplementation(() => "teste"),
+		carregarData: jest.fn().mockImplementation(() => "teste"),
+	}),
+}));
 
-        /* it("Deve renderizar texto no Box", () => {
-            renderComponent();
-            expect(screen.getByText("Nenhum processo encontrado")).toBeInTheDocument();
-        }) */
-
-        /* it("Deve renderizar botao de adicionar processo", () => {
-
-        })
-
-        it("Deve chamar funcao no botao de adicionar processo", () => {
-
-        })
-
-        it("Deve renderizar modal de processos", () => {
-
-        })
-
-        it("Deve chamar funcao onClose no modal de processo", () => {
-
-        }) */
+const renderComponent = () => render(<Home />)
 
 
+// beforeEach(() => {
+// 	const useDataContext = () 
+//   const { stateLoading, setStateLoading, listaProcesso, carregarData } = useDataContext();
+// })
+	
+describe("Home component", () => {
+	it("Deve renderizar texto com nenhum processo encontrado", () => {
 
-    })
+		
+		renderComponent();
+		expect(screen.getByText("Nenhum processo encontrado")).toBeInTheDocument();
+	})
+
+	/* it("Deve renderizar texto no Box", () => {
+			renderComponent();
+			expect(screen.getByText("Nenhum processo encontrado")).toBeInTheDocument();
+	}) */
+
+	/* it("Deve renderizar botao de adicionar processo", () => {
+
+	})
+
+	it("Deve chamar funcao no botao de adicionar processo", () => {
+
+	})
+
+	it("Deve renderizar modal de processos", () => {
+
+	})
+
+	it("Deve chamar funcao onClose no modal de processo", () => {
+
+	}) */
+})
