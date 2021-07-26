6888424a3b86607248cc0d69c4ae2f4d3cd10db7
Cristian Ogliari <cogliari@gmail.com>
2021-07-25 23:45:05 -0300

    criacao: adicionado teste de card de processo

--- Projetos/FrontEnd/front_processos/src/pages/Home/Home.test.js
+++ Projetos/FrontEnd/front_processos/src/pages/Home/Home.test.js
@@ -1,54 +1,66 @@
 import { render, screen } from "@testing-library/react";
 
 import { Home } from "./index";
-import { useDataContext } from "../../utils/context/DataContext";
-// jest.mock("../../utils/context/DataContext");
 
-jest.mock("../../utils/context/DataContext", () => ({
-	...jest.requireActual("../../utils/context/DataContext"),
-	useDataContext: () => ({
-		listaProcesso: [],
-		listaAssunto: [],
-		listaInteressado: [],
-		recarregarProcessos: jest.fn().mockImplementation(() => "teste"),
-		carregarData: jest.fn().mockImplementation(() => "teste"),
-	}),
-}));
+jest.mock("../../utils/context/DataContext");
 
 const renderComponent = () => render(<Home />)
+const dataContext = require("../../utils/context/DataContext");
 
+describe("Home component sem processos", () => {
+	beforeEach(() => {
+		dataContext.useDataContext.mockReturnValue({
+			listaProcesso: [],
+			listaAssunto: [],
+			listaInteressado: [],
+			recarregarProcessos: jest.fn().mockImplementation(() => ""),
+			carregarData: jest.fn().mockImplementation(() => ""),
+		})
+	})
 
-// beforeEach(() => {
-// 	const useDataContext = () 
-//   const { stateLoading, setStateLoading, listaProcesso, carregarData } = useDataContext();
-// })
-	
-describe("Home component", () => {
 	it("Deve renderizar texto com nenhum processo encontrado", () => {
 
-		
 		renderComponent();
 		expect(screen.getByText("Nenhum processo encontrado")).toBeInTheDocument();
 	})
+})
 
-	/* it("Deve renderizar texto no Box", () => {
-			renderComponent();
-			expect(screen.getByText("Nenhum processo encontrado")).toBeInTheDocument();
-	}) */
-
-	/* it("Deve renderizar botao de adicionar processo", () => {
-
+describe("Home component com processos", () => {
+	beforeEach(() => {
+		dataContext.useDataContext.mockReturnValue({
+			listaProcesso: [{  
+			"id": 1,
+			"nuprocesso": 1,
+			"sgorgaosetor": "SOFT",
+			"nuano": "2021",
+			"chaveprocesso": "SOFT 1/2021",
+			"descricao": "Fulano deseja autorizacao para corte de arvore frutifera",
+			"cdassunto": {
+				"id": 1,
+				"descricao": "Autorização para Corte de Árvores - Área Pública",
+				"dtcadastro": "2021-05-23",
+				"flativo": "S"
+			},
+			"cdinteressado": {
+				"id": 1,
+				"nminteressado": "Fulano de Tal",
+				"nuidentificacao": "46200016003",
+				"dtnascimento": "2000-01-01",
+				"flativo": "S"
+			}
+		}],
+			listaAssunto: [],
+			listaInteressado: [],
+			recarregarProcessos: jest.fn().mockImplementation(() => ""),
+			carregarData: jest.fn().mockImplementation(() => ""),
+		})
 	})
 
-	it("Deve chamar funcao no botao de adicionar processo", () => {
+	it("Deve renderizar card de processo", () => {
+		renderComponent();
 
+		expect(screen.getByText("SOFT 1/2021")).toBeInTheDocument();
+		expect(screen.getByText("2021")).toBeInTheDocument();
+		expect(screen.getByText("Autorização para Corte de Árvores - Área Pública")).toBeInTheDocument();
 	})
-
-	it("Deve renderizar modal de processos", () => {
-
-	})
-
-	it("Deve chamar funcao onClose no modal de processo", () => {
-
-	}) */
 })
